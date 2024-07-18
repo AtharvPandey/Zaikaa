8a493d042cb4f4dcb6020111ee126697c3adffa1
AtharvPandey <atharvrajendrapandey@gmail.com>
2024-07-19 01:15:21 +0530

    make navbar frontend

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("Home");
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>offers</li>
-        <li>contact us</li>
+        <li
+          className={menu === "Home" ? "active" : ""}
+          onClick={() => setMenu("Home")}
+        >
+          Home
+        </li>
+        <li
+          className={menu === "Menu" ? "active" : ""}
+          onClick={() => setMenu("Menu")}
+        >
+          Menu
+        </li>
+        <li
+          className={menu === "Offers" ? "active" : ""}
+          onClick={() => setMenu("Offers")}
+        >
+          Offers
+        </li>
+        <li
+          className={menu === "Contact-us" ? "active" : ""}
+          onClick={() => setMenu("Contact-us")}
+        >
+          Contact us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
