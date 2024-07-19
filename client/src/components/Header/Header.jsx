a028c9a2724e26efbba7c1d00b077fb33b7dd2b0
AtharvPandey <atharvrajendrapandey@gmail.com>
2024-07-19 10:52:10 +0530

    Update Home banner in Header file

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Swad Bhari Seva, Turant Aapke Darwaze Par!</h2>
+        <p>
+          Craving a hearty meal? Zaikaa delivers your favorite local dishes hot
+          and fresh, straight to your door. Experience the perfect blend of
+          taste and convenience with our rapid delivery service. Indulge in
+          deliciousness—order with Zaikaa today!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
