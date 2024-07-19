03e4002fb0558926866cb7c182dd6967baff967f
AtharvPandey <atharvrajendrapandey@gmail.com>
2024-07-19 16:54:05 +0530

    updated Home HEader and cart functionality

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Header.css";
+
 const Header = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    video.currentTime = 6.7; // Set the start time to 6.5 seconds
+
+    const handleTimeUpdate = () => {
+      if (video.currentTime >= 54.5) {
+        video.pause(); // Pause the video at 54.5 seconds
+        video.currentTime = 6.7; // Reset to start time
+        video.play(); // Restart the video from 6.5 seconds
+      }
+    };
+
+    video.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      video.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, []);
+
   return (
     <div className="header">
+      <video className="header-video" autoPlay muted loop ref={videoRef}>
+        <source src="/videoplayback.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
       <div className="header-contents">
         <h2>Swad Bhari Seva, Turant Aapke Darwaze Par!</h2>
         <p>
