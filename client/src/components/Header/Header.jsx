e6e1073ee1f2e425d7c5f30b607f68c56dd85b93
AtharvPandey <atharvrajendrapandey@gmail.com>
2024-07-21 12:58:26 +0530

    update header text

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
         <h2>Swad Bhari Seva, Turant Aapke Darwaze Par!</h2>
         <p>
           Craving a hearty meal? Experience the perfect blend of taste and
-          convenience with our rapid delivery service. Indulge in
-          deliciousness—order with Zaikaa today!
+          convenience with our rapid delivery service. Indulge in deliciousness
+          <br /> - order with Zaikaa today!
         </p>
         <button>View Menu</button>
       </div>
