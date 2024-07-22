6fb78e7d120408fb7d7630155d402afc0bcdc7ba
Atharv Pandey <72992789+AtharvPandey@users.noreply.github.com>
2024-07-22 14:55:55 +0530

    Update orderRoute.js

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,20 +1,13 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import {
-  placeOrder,
-  verifyOrder,
-  verifyPayment,
-} from "../controllers/orderController.js";
+import { placeOrder, verifyOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 // Place order
 orderRouter.post("/place", authMiddleware, placeOrder);
 
-// Verify payment
-orderRouter.post("/verify-payment", verifyPayment);
-
-// Verify order
+//Verify order
 orderRouter.post("/verify", verifyOrder);
 
 export default orderRouter;
