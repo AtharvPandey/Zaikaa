9a7bdea7473e7665fc4b9863a0550e304eeb4c7a
Atharv Pandey <72992789+AtharvPandey@users.noreply.github.com>
2024-07-22 15:01:39 +0530

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://zaikaa.vercel.app";
 
   try {
     const { userId, items, amount, address } = req.body;
